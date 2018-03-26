da1ece42af530dd4bde67b87fb0c47596026d844
ChernovAndrey <chernov.andrey.98@mail.ru>
2018-03-26 19:56:30 +0300

    add ecnoders

--- LipReading.py
+++ LipReading.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar 25 13:41:56 2018
+Created on Mon Mar 26 17:45:36 2018
 
 @author: andrey
 """
@@ -13,19 +13,41 @@ import torch.nn.functional as F
 
 batchNorm1 = nn.BatchNorm2d(96)
 batchNorm2 = nn.BatchNorm2d(256)
-class Net(nn.Module):
+class EncoderRNN(nn.Module):
 
     def __init__(self):
-        super(Net, self).__init__()
+        super(EncoderRNN, self).__init__()
+        #init parameters
+        self.hidden_size=256
         
+        # CNN (EF)
         self.conv1 = nn.Conv2d(in_channels=5,out_channels=96,kernel_size=(3,3),stride=(1,1), padding=(1,1))
         self.conv2 = nn.Conv2d(in_channels=96,out_channels=256,kernel_size=(3,3), padding =1, stride=(2,2))
         self.conv3 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3,3), padding=1)
         self.conv4 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3),padding=1)
         self.conv5 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3),padding=1)
         self.fc6 = nn.Linear(32768, 512) #512*8*8=32768 перейдет в 512
+        
+        # LSTM
+        self.lstm1=nn.LSTM(512,self.hidden_size)
+        self.lstm2=nn.LSTM(self.hidden_size,self.hidden_size)
+        self.lstm3=nn.LSTM(self.hidden_size,self.hidden_size)
+        
+    def forward(self,input,hidden1,hidden2,hidden3):
+        output = self.CNN(input)            
+        
+        output=output.view(1,1,512)
+        output,hidden1=self.lstm1(output,hidden1)
+        output,hidden2=self.lstm2(output,hidden2)
+        output,hidden3=self.lstm3(output,hidden3)
+        
+        return output,hidden1,hidden2,hidden3
+        
+    def initHidden(self):
+        return (torch.autograd.Variable(torch.randn(1, 1, self.hidden_size)),
+         torch.autograd.Variable(torch.randn((1, 1, self.hidden_size))))
             
-    def forward(self, x):
+    def CNN(self, x):
          #1   
          print(x.shape)
          x = F.relu(self.conv1(x))
@@ -70,16 +92,14 @@ class Net(nn.Module):
 
 
 
-net = Net()
-print(net)
+encoder = EncoderRNN()
+print(encoder)
 
 
 input = Variable(torch.randn(1,5,120, 120))
-out = net(input)
-print(out.shape) 
-#%%
-#%%
-for parameter in net.parameters():
-    print(parameter.shape)
-#%%
-   
+hidden1 = encoder.initHidden()
+hidden2 = encoder.initHidden()
+hidden3 = encoder.initHidden()
+out,hidden1,hidden2,hidden3 = encoder(input,hidden1,hidden2,hidden3)
+print(out.shape)
+#%% 
