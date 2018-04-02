58467ecba66b3dd6321341316b0a2e3515fec809
ChernovAndrey <chernov.andrey.98@mail.ru>
2018-04-02 21:57:46 +0300

    fix decoder

--- LipReading.py
+++ LipReading.py
@@ -65,7 +65,7 @@ class EncoderRNN(nn.Module):
         output,hidden=self.lstm1(input,hidden)
         print("outRNN",output.shape)
         print("hiddenRNN",hidden[0].shape)
-        return output,hidden[0]
+        return output,hidden
     def initHidden(self):
         return (torch.autograd.Variable(torch.randn(1, 1, self.hidden_size)),
          torch.autograd.Variable(torch.randn((1, 1, self.hidden_size))))
@@ -125,8 +125,12 @@ class DecoderRNN(nn.Module):
         self.output_encoder_size=10  #потом другая будет
         self.hidden_size=256
         self.lstm1=nn.LSTM(36,self.hidden_size) #кол-во букв в русском алфавите = 33 и еще + 3.
-        self.lstm2=nn.LSTM(self.hidden_size,self.hidden_size)
-        self.lstm3=nn.LSTM(self.hidden_size,self.hidden_size)
+#        self.lstm2=nn.LSTM(self.hidden_size,self.hidden_size)
+#        self.lstm3=nn.LSTM(self.hidden_size,self.hidden_size)
+        
+        
+        #attention
+        
         
         
         #MLP
@@ -135,21 +139,34 @@ class DecoderRNN(nn.Module):
         self.fc2=nn.Linear(self.MLP_hidden_size,36)
     def forward(self,Y,hidden):
         
-        output,hidden=self.lstm1(Y,hidden)
-        
-        print(output.shape)
-        print(hidden.shape)
-
+#        output = F.relu(output)  в статье его еще пропустили через relu
+#        output,hidden=self.lstm1(Y,hidden)
+#        
+#        print(output.shape)
+#        print(hidden.shape)
+#        
+#        output = F.relu(output) # так было в статье про машинный перевод
+        #hidden= torch.unsqueeze(hidden,0) # то есть размерность 1*5*120*120           
+        output, hidden = self.lstm1(Y, hidden)
+#        output = F.softmax(self.out(output[0]))
+#        return output, hidden
 #   осталось написать atention и MLP       
         Y = output # while atention skip
         
-    
-        Y = F.relu(self.fc1(Y))    
-        Y = self.fc2(Y)    
-        return F.log_softmax(Y,dim=1),C,hidden1,hidden2,hidden3  #         разобраться с softmax!
+        output=Variable(torch.FloatTensor(Y.shape[0],36).zero_())  # 36- размер алфавита
+        for i in range(len(Y)):
+            yi=torch.unsqueeze(Y[i],0)
+            yi = F.relu(self.fc1(yi))    
+            yi = self.fc2(yi)
+            output[i]=yi        
         
-#    
-#    def attention(hidden,outsEncoder):# вообще бред скорее всего
+    
+        print(output.shape)
+        return F.log_softmax(output, dim=1),hidden
+       # return F.log_softmax(Y,dim=1),C,hidden1,hidden2,hidden3  #         разобраться с softmax!
+        
+    
+#    def attention(hidden,outsEncoder):
 #        #все параметры будут обуучаться
 #        
 #        attn_W = Variable(torch.randn(256,256))# временно какая вообще размерность у нее должна быть?
@@ -166,7 +183,7 @@ class DecoderRNN(nn.Module):
 #            E[k] = F.softmax(E[k])    
 #        print(E.shape)
 #        return E
-        
+#        
     
 encoder = EncoderRNN()
 #print(encoder)
@@ -185,8 +202,12 @@ print(out.shape)
 #
 print("FINISH ENCODER")
 ##
-Y_answer=Variable(torch.torch.randn(seq_len,1,36)) # верные Y
 decoder= DecoderRNN()
-Y_NN = decoder(Y_answer,hidden) #Y_NN - что выдала NN
+
+count_character=100
+Y_answer=Variable(torch.torch.randn(count_character,1,36)) # верные Y
+out_RNN,hidden_RNN = decoder(Y_answer,hidden) #Y_NN - что выдала NN
+print(out_RNN.shape)
+#print(hidden_RNN.shape)
 #
 ##%% 
